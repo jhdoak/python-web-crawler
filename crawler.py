0691bc45ba87221102a450417b261778a9ec3d22
Justin Doak <justinhdoak@gmail.com>
2016-11-05 15:32:43 -0400

    Added while loop and parsing for anchor tags.

--- crawler.py
+++ crawler.py
@@ -2,3 +2,11 @@ import bs4.BeautifulSoup
 import requests
 
 to_visit = ["espn.com", "cnn.com"]
+
+while to_visit:
+    res = requests.get('http://' + to_visit.pop(0))
+    if res.status_code == 200:
+        result = res.text
+        soup = BeautifulSoup(result, 'lxml')
+        for a in soup.find_all('a', href=True):
+            to_visit.append(a['href'])
