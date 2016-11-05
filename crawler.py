abfed9fb5c34983d3075edc9b6390e6f81f73391
Justin Doak <justinhdoak@gmail.com>
2016-11-05 15:48:30 -0400

    Fixed BeautifulSoup import and added parens for Python3 print statement

--- crawler.py
+++ crawler.py
@@ -1,11 +1,11 @@
-import bs4.BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 
 to_visit = ["http://espn.com", "http://cnn.com"]
 
 while to_visit:
     next_to_visit = to_visit.pop(0)
-    print next_to_visit
+    print(next_to_visit)
     res = requests.get(next_to_visit)
     if res.status_code == 200:
         soup = BeautifulSoup(res.text, 'lxml')
